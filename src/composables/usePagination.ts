3384abed365b8e61d926c0932aeb135a86aedd28
Syntactic Sour <saypotato@proton.me>
2025-03-31 00:06:36 +0400

    feat: set default page

--- src/composables/usePagination.ts
+++ src/composables/usePagination.ts
@@ -1,1 +1,19 @@
-export function usePagination() {}
+import { computed, ref } from 'vue'
+
+export const DEFAULT_PAGINATION_LIMITS = new Set([10, 20, 30])
+
+export function usePagination({
+  limits = DEFAULT_PAGINATION_LIMITS,
+  show,
+  page = 1,
+}: {
+  show?: number
+  limits?: Set<number>
+  page?: number
+} = {}) {
+  const currentPage = ref(page)
+
+  return {
+    currentPage,
+  }
+}
